ce10236a0e4e7d429d163ff5c6b590e28234d0eb
admin <admin@DESKTOP-3VCH3QQ>
2019-03-19 11:11:03 +0530

    ArrayList

---
+++ CollectionsPrograms/src/p1/ArrayListEx1.java
@@ -0,0 +1,19 @@
+package p1;
+import java.util.ArrayList;
+
+//import java.util.*;
+public class ArrayListEx1 
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList a1=new ArrayList();
+		a1.add("hi");
+		a1.add(2.56f);
+		a1.add(0,4.5);
+		a1.add(0,2);
+		a1.add(0,2);
+		System.out.println(a1);
+	}
+
+}
